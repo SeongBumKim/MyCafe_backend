71c74ef1687ecff98d1541c1d24aa019ca992f6b
SeongBumkim <ghdigh123a@naver.com>
2021-10-13 17:51:28 +0900

    edit serializer

--- menu/serializer.py
+++ menu/serializer.py
@@ -6,9 +6,6 @@ from menu.models import Menu
 class MenuSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Menu
-        fields = ['id', 'name', 'price', 'stocked','category','imgUrl']
+        fields = ['id', 'name', 'price', 'stocked', 'category', 'imgUrl', 'ordered']
 
-class MenuSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Menu
-        fields = ['id', 'name', 'price', 'stocked','category','imgUrl']
+
